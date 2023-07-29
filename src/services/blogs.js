6bf93af945f8731207b397c4ed3f4c922a6e7f4d
achasek <chaseknieper@gmail.com>
2023-07-29 14:12:00 -0700

    init commit and setup

---
+++ src/services/blogs.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+// .then promise version
+// const getAll = () => {
+//   const request = axios.get(baseUrl)
+//   return request.then(response => response.data)
+// }
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll }

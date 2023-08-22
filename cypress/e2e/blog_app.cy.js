76b5038453edfff2b519e7703da93f93d0e1e616
achasek <chaseknieper@gmail.com>
2023-08-22 15:13:20 -0700

    added more cypress tests. Still have to create a test that checks if all of the blogs are ordered by likes; the most likes being the blog at the top

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -12,17 +12,36 @@ describe('Blog app', function() {
 
   // do need to visit page before every test tho
   beforeEach(function() {
-    cy.visit('http://localhost:3000');
+    // code prior to adding base url to the cypress.config.js
+    // cy.visit('http://localhost:3000');
+    cy.visit('');
   });
 
   it('front page can be opened', function() {
     cy.contains('Blogs');
   });
 
+  it('should not display login form by default', function() {
+    cy.contains('Login Form').should('not.exist');
+  });
+
   // this will have to be changed when you move the log in to a separate page later on
   // try changing the cy.visit('http://localhost:3000/login') or whatever the route for thatt new login location looks like
   it('login form can be opened', function() {
     cy.contains('Login').click();
+  });
+
+  // could write it.only(...) instead of just it
+  // this would make cypress ignore all the other tests and only run the test with it.only
+  it('login fails with wrong password', function() {
+    cy.contains('Login').click();
+    cy.get('#username').type('cypress');
+    cy.get('#password').type('wrong');
+    cy.get('#login-button').click();
+
+    cy.contains('Invalid Username or Password');
+    // just double checks that user is not logged in: get('html') gets all visible html on the page
+    cy.get('html').should('not.contain', 'Welcome Cyrpress Test');
   });
 
   // before adding 2nd .click() and .contains('Welcome kyra'), credentials here do not need to match any existing credentials in db yet. It just tests to see if one can attempt to login, not if the login is valid yet
@@ -44,14 +63,16 @@ describe('Blog app', function() {
 
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.contains('Login').click();
-      cy.get('input:first').type('cypress');
-      cy.get('input:last').type('e2etest');
+      // cy.contains('Login').click();
+      // cy.get('input:first').type('cypress');
+      // cy.get('input:last').type('e2etest');
 
-      cy.get('#login-button').click();
+      // cy.get('#login-button').click();
+      // a custom cy function found in cypress/support/commands.js named login
+      cy.login({ username: 'cypress', password: 'e2etest' });
     });
 
-    it('a new blog can be created', function() {
+    it('a new blog can be created through the UI', function() {
       cy.contains('Post New Blog').click();
       cy.get('.title').type('a blog created by cypress');
       cy.get('.author').type('author: cypress');
@@ -63,19 +84,52 @@ describe('Blog app', function() {
       cy.contains('a blog created by cypress');
     });
 
-    it('the new blog can have its details viewed', function() {
-      cy.contains('a blog created by cypress');
-      cy.contains('Show Details').click();
+    describe('and when a blog exists', function() {
+      // this beforeEach is not really needed, but just to display cypress custom commands
+      beforeEach(function() {
+        cy.createBlog({
+          title: 'a 2nd blog',
+          author: 'cypress',
+          url: 'www.cypress.com'
+        });
+      });
 
-      cy.contains('author: cypress');
-    });
+      it('the new blog can have its details viewed', function() {
+        cy.contains('a blog created by cypress');
+        cy.contains('Show Details').click();
 
-    it('a user can delete their own blog', function() {
-      cy.contains('a blog created by cypress');
-      cy.contains('Show Details').click();
-      cy.get('#delete-button').click({ force: true });
+        cy.contains('author: cypress');
+      });
 
-      cy.contains('a blog created by cypress').should('not.exist');
+      it('the blog can be liked', function() {
+        cy.contains('a blog created by cypress');
+        cy.contains('Show Details').click();
+        cy.contains('Like').click();
+
+        cy.contains('1 likes');
+      });
+
+      it('a user can delete their own blog', function() {
+        cy.contains('a blog created by cypress');
+        cy.contains('Show Details').click();
+        cy.get('#delete-button').click({ force: true });
+
+        cy.contains('a blog created by cypress').should('not.exist');
+      });
+
+      it('a logged  out user cannot see the delete button to a blog', function() {
+        cy.contains('Logout').click();
+        cy.contains('Show Details').click();
+        cy.contains('Delete').should('not.exist');
+      });
     });
   });
 });
+
+
+// can chain methods for a very refined search query
+// for ex. in the failed login test
+// cy.get('.error')
+// .should('contain', 'wrong credentials')
+// .and('have.css', 'color', 'rgb(255, 0, 0)')
+// .and('have.css', 'border-style', 'solid')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,36 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+// A NOTE ABOUT COMMANDS
+// in cyrpress, when writing tests in the front end, it has access to the UI
+// and can be used similarly to how an actual user would use the app
+// but when writing custom commands, we should utilize this space for
+// for making requests directly to the backend, so when we have the custom
+// function for creating a blog, we need to include the auth header and
+// to be accepted by the backend whereas in the frontend tests, we do not
+// need to write code for the auth header since the frontend deals
+// with attaching the auth header for us
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedAppUser', JSON.stringify(body));
+    cy.visit('http://localhost:3000');
+  });
+});
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedAppUser')).token}`
+    }
+  });
+
+  cy.visit('http://localhost:3000');
+});
